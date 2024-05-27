b187b93caed8f311ff0f49bdbb1d6c19885d36a1
Mehul Olakiya <mr.olakiya@1523@gmail.com>
2024-05-27 23:51:14 +0530

    [WIP] add style in main screen

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,41 @@
-import { View,Text } from "react-native";
+import { View,Text ,Pressable, StyleSheet} from "react-native";
 
 
-function PrimaryButton ({children}) {
-  return <View>
-   <Text>{children}</Text>
+function PrimaryButton ({children , onPress}) {
+
+ function pressHandler() {
+  console.log('Pressed')
+ }
+
+  return (
+  <View style={styles.buttonOuterContainer}>
+  <Pressable style={({pressed})=>pressed ? [styles.buttonInnerContainer,styles.preesed]:styles.buttonInnerContainer} onPress={onPress} android_ripple={{color:"#640233"}}>
+   <Text style={styles.buttonText}>{children}</Text>
+  </Pressable>
   </View>
+  )
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer:{
+    borderRadius:28,
+    margin:4,
+    overflow:'hidden',
+    
+  },
+  buttonInnerContainer:{
+    backgroundColor:"#72063c",
+    paddingVertical:8,
+    paddingHorizontal:16,
+    elevation:2
+  },
+  buttonText:{
+    textAlign:'center',
+    color:'white'
+  },
+  preesed:{
+    opacity:0.55
+  }
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,37 +1,83 @@
-import { TextInput , View , StyleSheet} from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
 
-function StartGameScreen () {
-return (
+function StartGameScreen() {
+const [enteredNumber,setEnteredNumber] = useState('')
+
+ 
+function numberHandler (enteredNumber) {
+    setEnteredNumber(enteredNumber)
+}
+
+function confirmInputHandler(){
+const choosenNumber = parseInt(enteredNumber)
+
+if(isNaN(choosenNumber) || choosenNumber <=0 || choosenNumber > 99){
+    // show Alret
+    Alert.alert('Invalid Number !','Number has to be a number between 1 to 99.',
+        [{text:'Okay',style:'destructive',onPress:resetInputHandler}]
+    )
+    return;
+}
+}
+
+function resetInputHandler(){
+    setEnteredNumber('')
+}
+
+  return (
     <View style={styles.inputContainer}>
-        <TextInput style={styles.numberInput} maxLength={2} inputMode="numeric" />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberHandler}
+        value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
-)
+  );
 }
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer :{
-        marginTop:100,
-        padding:16,
-        marginHorizontal:26,
-        backgroundColor:'#72063c',
-        borderRadius:8,
-        elevation:10,
-    },
-    numberInput:{
-       height:50,
-       width:60,
-       fontSize:32,
-       borderBottomColor:'#ddb52f',
-       borderBottomWidth:2,
-       color:'#ddb52f',
-       marginVertical:8,
-       fontWeight:'bold',
-       textAlign:'center'
-    }
-}) 
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    padding: 16,
+    marginHorizontal: 26,
+    backgroundColor: "#3b021f",
+    borderRadius: 8,
+    elevation: 10,
+  },
+  numberInput: {
+    height: 50,
+    width: 60,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+   buttonsContainer:{
+    flexDirection:'row'
+   },
+   buttonContainer:{
+    flex:1
+   }
+});
